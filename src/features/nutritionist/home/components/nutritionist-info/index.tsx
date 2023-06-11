e1cd296b950e394dff5c922b46a8d2e4939a8476
Edvan de Matos Santos <edvan.stt02@gmail.com>
2023-06-11 15:07:48 -0300

    build: :construction_worker: expo eas build

--- src/features/nutritionist/home/components/nutritionist-info/index.tsx
+++ src/features/nutritionist/home/components/nutritionist-info/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { NutritionistInfoContainer } from './styles';
 import { useUserInfo } from '@hooks/user/use-user-info';
 import { Text } from '@styles/components/text';
+import { useTokenStore } from '@store/token';
 
 const NutritionistInfo = () => {
   const { user } = useUserInfo();
 
+  const { setToken } = useTokenStore();
+
   return (
     <NutritionistInfoContainer>
-      <Text variant="headlineSmall" fontWeight="600">
+      <Text
+        variant="headlineSmall"
+        fontWeight="600"
+        onPress={() => {
+          setToken('');
+        }}
+      >
         Olá, {user?.person?.name || 'Nutricionista'}
       </Text>
     </NutritionistInfoContainer>
